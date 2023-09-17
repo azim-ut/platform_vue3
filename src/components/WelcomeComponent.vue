ee72915686c48fb1860cd3a27e7b0b75418a0b68
Aleksandr Azimut <9420183@gmail.com>
2023-09-18 01:58:18 +0300

    clear redundant code

---
+++ src/components/WelcomeComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Welcome
+  </div>
+</template>
+
+<script lang="ts">
+
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {
+  }
+})
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <WelcomeComponent />
+  </main>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import WelcomeComponent from "@/components/WelcomeComponent.vue";
+
+export default defineComponent({
+  components: {
+    WelcomeComponent
+  }
+})
+</script>
